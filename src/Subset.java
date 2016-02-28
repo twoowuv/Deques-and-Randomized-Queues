8afcd7443d965b25c63c236cd8287c3f0b69a188
twoowuv <importantaffairs@gmail.com>
2016-02-28 22:49:37 +1300

    Add subset class

---
+++ src/Subset.java
@@ -0,0 +1,36 @@
+import java.lang.IndexOutOfBoundsException;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * Takes a command line argument as the number of items to print out at random.
+ * Then takes in N strings and prints out k of those strings at random.
+ * Because we are using the RandomizedQueue they cannot be repeated because they are
+ * removed once they are printed.
+ */
+public class Subset {
+
+    public static void main(String[] args) {
+
+        RandomizedQueue<String> bag = new RandomizedQueue<String>();
+
+        // The number of items to print
+        int k = Integer.parseInt(args[0]);
+
+        if (k < 1) {
+            throw new IndexOutOfBoundsException("There's no point reading 0 strings out.");
+        }
+
+        while (!StdIn.isEmpty()) {
+            String str = StdIn.readString();
+
+            bag.enqueue(str);
+        }
+
+        for (int i = 0; i < k; i++) {
+            StdOut.println(bag.dequeue());
+        }
+
+    }
+}
